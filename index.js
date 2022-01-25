3639787c203d36faf85e0b4c22ad5b51630b750c
Anthony Bonsi <xbonsi96@gmail.com>
2022-01-24 17:10:20 -0700

    commit tree e9cb1591c6196ab1ef137278584a3bed21a5d1bb

---
+++ index.js
@@ -0,0 +1,20 @@
+function receivesAFunction(callback) {
+   callback();
+}
+
+function returnsANamedFunction() {
+   return returnName 
+}
+
+function returnName() {
+
+}
+
+function returnsAnAnonymousFunction() {
+    return function() {
+
+    }
+}
+
+
+
